f0f658fdcbfe187e70b3b385ed76657c9749a4e0
Angelos <SolegnaT@users.noreply.github.com>
2024-08-24 21:18:28 +0000

    Update mario

--- emscripten/output.js
+++ emscripten/output.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpfxrjtxh7.js
+// include: /tmp/tmpy9ersyzo.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -220,21 +220,21 @@ Module['FS_createPath']("/", "assets", true, true);
 
   })();
 
-// end include: /tmp/tmpfxrjtxh7.js
-// include: /tmp/tmp4_z0a7ld.js
+// end include: /tmp/tmpy9ersyzo.js
+// include: /tmp/tmp1i_k9jo5.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp4_z0a7ld.js
-// include: /tmp/tmpiyry2jl4.js
+  // end include: /tmp/tmp1i_k9jo5.js
+// include: /tmp/tmplkfkyusw.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpiyry2jl4.js
+  // end include: /tmp/tmplkfkyusw.js
 
 
 // Sometimes an existing Module object exists with properties
